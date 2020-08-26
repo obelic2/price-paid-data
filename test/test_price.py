2f29ff2e41e230cfdb95a205d4363f11839c89f4
Ognjen Belic <obelic2@gmail.com>
2020-08-26 20:11:46 +0100

    Fix test case with updated naming

--- test/test_price.py
+++ test/test_price.py
@@ -1,41 +1,39 @@
 # Test suite for price paid data
 
 import pytest
-from app.app import CreateJSONObject
+from app.app import CreateAddressObject
 
 
 def test_csv_to_json():
     input_line = (
         '"{A96E4ACB-D1DA-9205-E053-6C04A8C0DA09}","240000","2004-06-22 00:00",'
         '"BS40 5JL","S","N","F","3","","CAMBRIDGE COURT","WRINGTON","BRISTOL",'
-        '"NORTH SOMERSET","NORTH SOMERSET","A","A"')
+        '"NORTH SOMERSET","NORTH SOMERSET","A","A"'
+    )
 
-    expected = {
-        '3 CAMBRIDGE COURT WRINGTON BRISTOL NORTH SOMERSET NORTH SOMERSET BS40 5JL': [
-            {
-                'transaction_id': '{A96E4ACB-D1DA-9205-E053-6C04A8C0DA09}',
-                'price': '240000',
-                "transfer_date": '2004-06-22 00:00',
-                'postcode': 'BS40 5JL',
-                'property_type': 'S',
-                'old_new': 'N',
-                'duration': 'F',
-                'paon': '3',
-                'saon': '',
-                'street': 'CAMBRIDGE COURT',
-                'locality': 'WRINGTON',
-                'town_city': 'BRISTOL',
-                'district': 'NORTH SOMERSET',
-                'county': 'NORTH SOMERSET',
-                'ppd_category_type': 'A',
-                'record_status': 'A'
-            }
-        ]        
-    }
+    expected = (
+        "3 CAMBRIDGE COURT WRINGTON BRISTOL NORTH SOMERSET NORTH SOMERSET BS40 5JL",
+        {
+            "transaction_id": "{A96E4ACB-D1DA-9205-E053-6C04A8C0DA09}",
+            "price": "240000",
+            "transfer_date": "2004-06-22 00:00",
+            "postcode": "BS40 5JL",
+            "property_type": "S",
+            "old_new": "N",
+            "duration": "F",
+            "paon": "3",
+            "saon": "",
+            "street": "CAMBRIDGE COURT",
+            "locality": "WRINGTON",
+            "town_city": "BRISTOL",
+            "district": "NORTH SOMERSET",
+            "county": "NORTH SOMERSET",
+            "ppd_category_type": "A",
+            "record_status": "A",
+        },
+    )
 
-    pipe = CreateJSONObject()
+    pipe = CreateAddressObject()
     result = pipe.process(input_line)
 
     assert next(result) == expected
-
-

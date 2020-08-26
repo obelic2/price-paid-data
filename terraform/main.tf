a50f0d901bd45164b85342f9777d211a6412ac86
Ognjen Belic <obelic2@gmail.com>
2020-08-26 16:46:13 +0100

    Add GCS bucket to terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -16,26 +16,32 @@ provider "google" {
   zone = var.zone
 }
 
-resource "google_compute_instance" "vm_instance" {
-  name         = "beam-runner"
-  machine_type = "f1-micro"
-  zone = var.zone
+# resource "google_compute_instance" "vm_instance" {
+#   name         = "beam-runner"
+#   machine_type = "f1-micro"
+#   zone = var.zone
 
-  boot_disk {
-    initialize_params {
-      image = "debian-cloud/debian-9"
-    }
-  }
+#   boot_disk {
+#     initialize_params {
+#       image = "debian-cloud/debian-9"
+#     }
+#   }
 
-  network_interface {
-    # A default network is created for all GCP projects
-    network = google_compute_network.vpc_network.self_link
-    access_config {
-    }
-  }
-}
+#   network_interface {
+#     # A default network is created for all GCP projects
+#     network = google_compute_network.vpc_network.self_link
+#     access_config {
+#     }
+#   }
+# }
 
-resource "google_compute_network" "vpc_network" {
-  name                    = "terraform-network"
-  auto_create_subnetworks = "true"
+# resource "google_compute_network" "vpc_network" {
+#   name                    = "terraform-network"
+#   auto_create_subnetworks = "true"
+# }
+
+resource "google_storage_bucket" "beam_storage" {
+  name = "uk-housing-prices"
+  location = var.region
+  project = var.gcp_project  
 }
